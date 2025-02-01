369628375345233813b512e355c129cc1ca39d14
VIJAY071102 <jsvijayjsv@gmail.com>
2025-02-01 22:45:46 +0530

    updates

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
-import add_product_icon from "../../assets/add_product.png";
-import list_product_icon from "../../assets/product_list.png";
+import add_product from "../../assets/add_product.png";
+import list_product from "../../assets/product_list.png";
 import { Link } from "react-router-dom";
 function Sidebar() {
   return (
     <div className="sidebar">
       <Link to={"/addproduct"} style={{ textDecoration: "none" }}>
         <div className="sidebar-item">
-          <img src={add_product_icon} alt="" />
+          <img src={add_product} alt="" />
           <p>Add Product</p>
         </div>
       </Link>
       <Link to={"/listproduct"} style={{ textDecoration: "none" }}>
         <div className="sidebar-item">
-          <img src={list_product_icon} alt="" />
+          <img src={list_product} alt="" />
           <p>Product List</p>
         </div>
       </Link>
